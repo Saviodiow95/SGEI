d74d3aeb3bb4fd291b242c95153253077f0e0cbd
Sávio Silva Moreira <saviodiowflamengo@gmail.com>
2020-12-18 13:22:19 -0300

    cadastro de usuario

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
+from accounts.views import create_user_arquivo
+
 app_name='accounts'
 
 urlpatterns = [
-
+    path('user/arquivo/', create_user_arquivo, name='create_user_arquivo'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.hashers import make_password
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 
 # Create your views here.
+from accounts.models import User
+
+
+def create_user_arquivo(request):
+    context = {}
+    fs = FileSystemStorage()
+
+    if (request.method == 'POST'):
+
+        arq = request.FILES['arquivo']
+        fs.save('accounts/'+arq.name, arq)
+        #print(arq.read())
+        dados = open('media/accounts/'+arq.name, 'r')
+
+        for l in dados:
+
+            aux = l.split()
+            user = User()
+            user.username = aux[0]
+            user.email = aux[1]
+            user.cpf = aux[2]
+            user.password = make_password(aux[2])
+            user.save()
+
+
+    return render(request,'arquivos/cad.html',context)
